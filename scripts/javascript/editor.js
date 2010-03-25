5f9a06bab753052bb486f58b78928ecc36d739d1
Jessica B Hamrick <jhamrick@mit.edu>
2010-03-25 02:48:46 -0400

    Don't have the WYSIWYG editor and code editor be the same div

--- scripts/javascript/editor.js
+++ scripts/javascript/editor.js
@@ -9,15 +9,21 @@ function switchToSource() {
     wysiwyg_button = document.getElementById("wysiwyg_button");
     source_button.disabled = "true";
     wysiwyg_button.disabled = "";
+
+    $('#source_editor').show();
+    $('#wysiwyg_editor').hide();
 }
 
 function switchToWYSIWYG() {
-    editor = CKEDITOR.replace("editor");
+    editor = CKEDITOR.replace("wysiwyg_editor");
     
     source_button = document.getElementById("source_button");
     wysiwyg_button = document.getElementById("wysiwyg_button");
     source_button.disabled = "";
     wysiwyg_button.disabled = "true";
+
+    $('#source_editor').hide();
+    $('#wysiwyg_editor').show();
 }
 
 $(function () {
