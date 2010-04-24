0f6177fa74c4e3edaee7a0c23268727242ecc959
Alan Huang <cesium@mit.edu>
2010-04-24 17:24:26 -0400

    Fixed CGI?

---
+++ docs/get.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys
+import os
+
+print 'Content-type: application/pdf'
+print
+
+path = '/mit/mitex/web_scripts/docs/tex.pdf'
+delete = False
+if len(sys.argv) == 2:
+	arg = os.path.basename(sys.argv[1])
+	if arg.endswith('.pdf'):
+		path = os.path.join('/mit/mitex/web_scripts/docs', arg)
+		delete = True
+sys.stdout.write(open(path, 'rb').read())
+if delete:
+	os.unlink(path)
