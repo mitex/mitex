6997548c80d0e9f9666dfe28e36d7d43dcd33b6b
Jason S Gross <jgross@mit.edu>
2010-06-15 03:07:02 -0400

    Replaced CKEditor with TinyMCE + math plugin

--- scripts/javascript/editor.js
+++ scripts/javascript/editor.js
@@ -34,12 +34,38 @@ function switchToSource() {
     reload_editarea();
 }
 
+function init_tinyMCE() {
+  tinyMCE.init({
+      mode : "textareas",
+      theme : "advanced",
+      theme_advanced_buttons1 : "fontselect,fontsizeselect,formatselect,bold,italic,underline,strikethrough,separator,sub,sup,separator,cut,copy,paste,undo,redo",
+      theme_advanced_buttons2 : "justifyleft,justifycenter,justifyright,justifyfull,separator,numlist,bullist,outdent,indent,separator,forecolor,backcolor,separator,hr,link,unlink,image,table,code,separator,asciimath,asciimathcharmap,asciisvg",
+      theme_advanced_buttons3 : "",
+      theme_advanced_fonts : "Arial=arial,helvetica,sans-serif,Courier New=courier new,courier,monospace,Georgia=georgia,times new roman,times,serif,Tahoma=tahoma,arial,helvetica,sans-serif,Times=times new roman,times,serif,Verdana=verdana,arial,helvetica,sans-serif",
+      theme_advanced_toolbar_location : "top",
+      theme_advanced_toolbar_align : "left",
+      theme_advanced_statusbar_location : "bottom",
+      plugins : 'safari,asciimath,asciisvg,table,inlinepopups',
+      
+      // If someone can get the following php file working on scripts, change the path to a local version (scripts/php/svgimg.php).
+      AScgiloc : 'http://www.imathas.com/editordemo/php/svgimg.php',			      //change me  
+      ASdloc : 'scripts/javascript/tiny_mce/plugins/asciisvg/js/d.svg',  //change me  	
+
+//      AScgiloc : 'scripts/php/svgimg.php',			      //change me  
+//      ASdloc : 'scripts/javascript/tiny_mce/plugins/asciisvg/js/d.svg',  //change me          
+  
+      content_css : "/css/content.css"
+  });
+}
+
 function switchToWYSIWYG() {
-    editor = CKEDITOR.replace("wysiwyg_editor");
+    //editor = CKEDITOR.replace("wysiwyg_editor");
+    init_tinyMCE();
     
     document.getElementById("source_button").disabled = "";
     document.getElementById("wysiwyg_button").disabled = "true";
 
+    $('#wysiwyg_editor').show();
     $('#source_editor').hide();
     resize_log();
 }
