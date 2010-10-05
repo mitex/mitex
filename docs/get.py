b7c2945f5e01a69dff8f12110133f17257db6643
Jason Gross <jasongross9+git@gmail.com>
2010-10-05 00:29:25 -0400

    Revert "Copied Alan's update to get.py to the production branch."
    This reverts commit 74172c1b73da537f06eaabcd773402c95426204a in favor of
    a cherry-pick.

--- docs/get.py
+++ docs/get.py
@@ -7,10 +7,10 @@ import os
 print 'Content-type: application/pdf'
 print
 
-path = 'tex.pdf'
+path = '/mit/mitex/web_scripts/docs/tex.pdf'
 delete = False
 if len(sys.argv) == 2:
-	arg = os.path.basename(sys.argv[1])
+	arg = os.path.join('/mit/mitex/web_scripts/docs', os.path.basename(sys.argv[1]))
 	if arg.endswith('.pdf') and os.path.exists(arg):
 		path = arg
 		delete = True
